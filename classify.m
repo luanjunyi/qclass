ef5c257c55a01c1932717eabc1c94e88692c1f28
Jerry <luanjunyi@gmail.com>
2013-05-26 17:02:25 +0800

    neural network iter=200 rate=81.2%

--- classify.m
+++ classify.m
@@ -8,7 +8,7 @@ disp(size(y));
 fflush(stdout);
 
 % y_result = rand_guess(X, y, X_query);
-y_result = logit_guess(X, y, X_query);
+y_result = neural_guess(X, y, X_query);
 
 % map 0, 1 back to -1 +1
 y_result = y_result * 2 - 1;
@@ -17,6 +17,3 @@ for i = 1:size(X_query_id, 2)
     fprintf(fout, '%s %+d\n', X_query_id{i}, y_result(i));
 end
 fclose(fout);
-
-
-
--- neural_guess.m
+++ neural_guess.m
@@ -1,19 +1,28 @@
 function y_label = neural_guess(X, y, X_query)
-  query_num = size(X_query, 1)
-  y_label = zeros(query_num, 1);
-
-  [m, n] = size(X);
-  X = [ones(m, 1) X];
-  initial_theta = zeros(n + 1, 1);
+  input_layer_size = size(X, 2);
+  hidden_layer_size = 25;
+  num_labels = 1;
   lambda = 1;
-  options = optimset('GradObj', 'on', 'MaxIter', 500000, 'Display','iter');
-  fprintf('Logistic regression start training\n');
-  fprintf('initial error:%f, press any key to continue...\n', lrCostFunction(initial_theta, X, y, lambda));
-  pause;
-  [theta, J, exit_flag] = fmincg(@(t)(lrCostFunction(t, X, y, lambda)), initial_theta, options);
 
-  p = predict(theta, X);
-  fprintf('Logistic regression train accuracy: %f\n', mean(double(p == y)) * 100);
-  X_query = [ones(query_num, 1) X_query];
-  y_label = predict(theta, X_query);
+  initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+  initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+  initial_nn_params = [initial_Theta1(:); initial_Theta2(:)];
+  costFunction = @(p) nnCostFunction(p, ...
+                                     input_layer_size, ...
+                                     hidden_layer_size, ...
+                                     num_labels, X, y, lambda);
+  options = optimset('MaxIter', 200);
+  
+  [nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+  
+  Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+
+  Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+  
+  pred = nn_predict(Theta1, Theta2, X);
+  fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+
+  y_label = nn_predict(Theta1, Theta2, X_query);
 end

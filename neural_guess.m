5e8d6cf047adb8243ebf10d5985f80d10349708a
Jerry <luanjunyi@gmail.com>
2013-05-29 21:42:03 +0800

    add python

--- neural_guess.m
+++ neural_guess.m
@@ -1,12 +1,16 @@
 function y_label = neural_guess(X, y, X_query)
+  y = y + 1;
+    
   input_layer_size = size(X, 2);
-  hidden_layer_size = 25;
-  num_labels = 1;
+  hidden_layer_size = 150;
+  num_labels = 2;
   lambda = 1;
 
   initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
   initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
   initial_nn_params = [initial_Theta1(:); initial_Theta2(:)];
+  
+  
   costFunction = @(p) nnCostFunction(p, ...
                                      input_layer_size, ...
                                      hidden_layer_size, ...
@@ -25,4 +29,5 @@ function y_label = neural_guess(X, y, X_query)
   fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
 
   y_label = nn_predict(Theta1, Theta2, X_query);
+  y_label = y_label - 1;
 end
